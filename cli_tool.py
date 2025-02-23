cabde575532191921cceedd56ef142e1505f8c86
RanaUniverse <RanaUniverse321@gmail.com>
2025-02-23 19:21:42 +0530

    added banaa showing funcion

--- cli_tool.py
+++ cli_tool.py
@@ -10,3 +10,18 @@ def count_characters():
         f"Hello User, The number of character in this sentence is:\n" f"{total_len}"
     )
     print(output_sen)
+
+
+def count_bananas():
+    user_input = input("Write any sentence here: ")
+    total_len = len(user_input)
+    bananas = "🍌" * total_len  # Repeat the 🍌 emoji based on input length
+
+    output_sen = (
+        f"Hello User, The number of characters in this sentence is: {total_len}\n"
+        f"Here are your bananas: \n{bananas}"
+    )
+    print(output_sen)
+
+
+# Call the function
